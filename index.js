25b28437e3d623f9b6deaa2b598c8aca5a1bad2f
jptechstack <jptechstack@email.com>
2026-07-14 23:14:50 -0300

    Trabalhando com Json

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-var name       = document.querySelector("#exampleInputName");
-var gender     = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth      = document.querySelector("#exampleInputBirth");
-var country    = document.querySelector("#exampleInputCountry");
-var email      = document.querySelector("#exampleInputEmail");
-var password   = document.querySelector("#exampleInputPassword");
-var photo      = document.querySelector("#exampleInputFile");
-var admin      = document.querySelector("#exampleInputAdmin");
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach(function(field, index){
 
@@ -15,17 +7,17 @@ fields.forEach(function(field, index){
         
         if (field.checked) {
             
-            console.log('SIM', field);
+            user[field.name] = field.value;
             
         }
 
     } else {
 
-        console.log('NÃO');
+        user[field.name] = field.value;
         
     }
-
-    //console.log(field.id, field.name, field.value, field.checked, index, );
     
 
 });
+
+console.log(user);
