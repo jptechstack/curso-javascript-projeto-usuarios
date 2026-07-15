542838892433b947b355d28d1543fa9628c346a0
jptechstack <jptechstack@email.com>
2026-07-15 11:37:36 -0300

    Trabalhando com arquivos - File Reader

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -15,10 +15,46 @@ class UserController {
             
             event.preventDefault();
 
-            this.addLine(this.getValues());
+            let values = this.getValues();
+
+            values.photo = "";
+
+            this.getPhoto((content)=>{
+                
+                values.photo = content;
+                
+                this.addLine(values);
+
+            });
+
+           
 
         });
 
+    }
+
+    getPhoto(callback) {
+        
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item=>{
+
+            if (item.name === 'photo') {
+
+               return item;
+
+            };
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = ()=>{
+
+            callback(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
     }
 
     getValues() {
@@ -55,16 +91,16 @@ class UserController {
         );
     }
 
-    addLine(user) {
+    addLine(dataUser) {
        
         this.tableEl.innerHTML = `
         
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                <td>${user.name}</td>
-                <td>${user.email}</td>
-                <td>${user.admin}</td>
-                <td>${user.birth}</td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${dataUser.admin}</td>
+                <td>${dataUser.birth}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
