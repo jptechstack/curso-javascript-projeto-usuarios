9a554fd0bf35e20a0d52518261b61685a2ed0f4f
jptechstack <jptechstack@email.com>
2026-07-14 22:58:46 -0300

    If e Else

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 
 fields.forEach(function(field, index){
 
-    console.log(field.id, field.name, field.value, field.checked, index, );
+    if (field.name == "gender" ) {
+        
+        if (field.checked) {
+            
+            console.log('SIM', field);
+            
+        }
+
+    } else {
+
+        console.log('NÃO');
+        
+    }
+
+    //console.log(field.id, field.name, field.value, field.checked, index, );
     
 
 });
