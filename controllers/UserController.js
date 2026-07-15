cf3b3cca0c1ebf50647c94d46ccc3318a15772de
jptechstack <jptechstack@email.com>
2026-07-15 14:27:47 -0300

    Ajustando a View do Projeto - Operador Ternário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -60,10 +60,20 @@ class UserController {
             };
 
             fileReader.onerror = (e)=>{
+
                 reject(e);
+                
             }
 
-            fileReader.readAsDataURL(file);
+            if (file) {
+
+                fileReader.readAsDataURL(file);
+
+            } else {
+
+                resolve('dist/img/boxed-bg.jpg');
+            }
+            
 
         });
         
@@ -82,6 +92,10 @@ class UserController {
                     user[field.name] = field.value;
                     
                 }
+
+            } else if (field.name == "admin") {
+
+                user[field.name] = field.checked;
 
             } else {
 
@@ -104,22 +118,24 @@ class UserController {
     }
 
     addLine(dataUser) {
-       
-        this.tableEl.innerHTML = `
+
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
         
-            <tr>
-                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
-        
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : "Não"}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+ 
         `;
+
+        this.tableEl.appendChild(tr);
 
     }
 
